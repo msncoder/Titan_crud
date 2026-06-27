b607eedc953692864ff287dc0bd9433c6980856b
saad <saad@authentiksolution.com>
2026-06-27 19:06:30 +0500

    create api done

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.api.todo import router as todo_router
+
+app = FastAPI()
+
+app.include_router(todo_router)
+
+
+@app.get("/")
+def home():
+    return {
+        "message":"hello"
+    }
